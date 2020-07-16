026704afac14bd19b9941196fe408856dafb0c34
Calli Oberembt <croberembt@gmail.com>
2020-07-16 14:07:25 -0500

    even-or-odd

---
+++ even-or-odd.js
@@ -0,0 +1,19 @@
+/*
+instructions: 
+Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+starting code: 
+function even_or_odd(number) {
+  // ...
+}
+
+solution below comments: 
+*/
+
+function even_or_odd(number) {
+    if (number % 2 === 0) {
+      return('Even');
+      } else {
+        return('Odd');
+      }
+  }
